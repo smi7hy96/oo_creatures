355465451f5b1839b15d0150402cc19726f5408e
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-05-30 23:37:49 +0100

    added tests for fly and squish

--- cockroach_class.py
+++ cockroach_class.py
@@ -1,5 +1,5 @@
 class Cockroach:
-    def __init__(self, size, colour, wings, species):
+    def __init__(self, size, colour, wings, species, status):
         self.size = size
         self.colour = colour
         self.wings = wings
--- cockroach_tests.py
+++ cockroach_tests.py
@@ -4,8 +4,18 @@ from cockroach_class import *
 
 class CockroachTest(unittest.TestCase):
     def setUp(self):
-        self.cockroach = Cockroach(5, 'black', True, 'american')
+        self.cockroach = Cockroach(5, 'black', True, 'american', 'alive')
 
+    def test_fly(self):
+        self.assertEqual(self.cockroach.fly(), 'RUN AWAY FROM IT!')
+        self.cockroach.size = 1
+        self.assertEqual(self.cockroach.fly(), 'SOMEONE SWAT IT!')
+
+    def test_squish(self):
+        self.assertEqual(self.cockroach.squish(0), "how did it not die")
+        self.cockroach.size = 1
+        self.assertEqual(self.cockroach.squish(0), "quick little bugger")
+        self.assertEqual(self.cockroach.squish(1), 'WE GOT IT')
 
 if __name__ == '__main__':
     unittest.main()
